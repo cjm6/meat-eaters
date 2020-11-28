ecb79674cb631af7b21dc59d36b39509aaea9130
cjm6 <1286761773@qq.com>
2020-11-28 15:48:50 +0800

    食荤者餐厅

---
+++ js/welcome/data-noteList.js
@@ -0,0 +1,349 @@
+var noteList = [{
+        "id": "5db7e12c000000000100575e",
+        "title": "招牌烧鸭",
+        "type": "normal",
+        "likes": 373,
+        "isLiked": true,
+        "cover": {
+            "url": "img/ms1.jpg",
+            "width": 1280,
+            "height": 1706,
+            "fileId": "63841e69-f915-3e92-b0e0-577835621815"
+        },
+        "user": {
+            "id": "5c315a550000000006031475",
+            "image": "https://img.xiaohongshu.com/avatar/5dce7f550eae5e0001fe48f3.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "daisy",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dcfc22e00000000010043b8",
+        "title": "吃了停不下来的霸王蟹❗️吃货无限回购款😋",
+        "type": "normal",
+        "likes": 364,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms2.jpg",
+            "width": 1280,
+            "height": 1705,
+            "fileId": "e03bef64-2eee-3789-ba48-da083135998e"
+        },
+        "user": {
+            "id": "5ac46cc0e8ac2b533d374630",
+            "image": "https://img.xiaohongshu.com/avatar/5ac46d1114de412eca1af482.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "瑾宁妈妈",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dcf5c2d0000000001009456",
+        "title": "🔥零厨艺❗️免打发❗️厨房小白也能轻松搞定的牛排🥞，新鲜美味",
+        "type": "normal",
+        "likes": 61,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms3.jpg",
+            "width": 1280,
+            "height": 1706,
+            "fileId": "7fcac969-592a-385d-bd27-a76d8e0c9dca"
+        },
+        "user": {
+            "id": "5b90881fd68e4900012d7a26",
+            "image": "https://img.xiaohongshu.com/avatar/5c62a25b6281e4000180add2.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "营养搭配均匀的早餐",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dceb7d00000000001003787",
+        "title": "北方的老铁们，这是你们喜欢的哦！",
+        "type": "normal",
+        "likes": 60,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms4.jpg",
+            "width": 1080,
+            "height": 1080,
+            "fileId": "63d94abd-b79e-32b5-97ea-47435c5e0ee0"
+        },
+        "user": {
+            "id": "5b2341bde8ac2b6cf730adf3",
+            "image": "https://img.xiaohongshu.com/avatar/5c773e991306d00001f850e8.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "妈咪很ok",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dce811f000000000100a3b5",
+        "title": "好吃不胖❗做法简单🔥超可爱营养早餐",
+        "type": "normal",
+        "likes": 60,
+        "isLiked": true,
+        "cover": {
+            "url": "img/ms5.jpg",
+            "width": 1280,
+            "height": 1706,
+            "fileId": "2821ce43-d7b8-3cb2-a2e3-9edb9157a103"
+        },
+        "user": {
+            "id": "5afea18511be1007defd9c1d",
+            "image": "https://img.xiaohongshu.com/avatar/5d5613671d314d0001fcf3eb.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "食荤者小厨房",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dd1b1680000000001006451",
+        "title": "虾饺，其实它是我的最爱哦！你们喜欢吗？",
+        "type": "normal",
+        "likes": 12,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms6.jpg",
+            "width": 1280,
+            "height": 960,
+            "fileId": "3fb03b6f-6e22-346a-a60e-21d506f026c7"
+        },
+        "user": {
+            "id": "599b00e750c4b45b6728b109",
+            "image": "https://img.xiaohongshu.com/avatar/5d85d5efd2c53d00017a0d9e.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "May娘",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5db7df5000000000010029fd",
+        "title": "牛油果美食！",
+        "type": "normal",
+        "likes": 345,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms7.jpg",
+            "width": 1280,
+            "height": 1280,
+            "fileId": "d61e95e7-2d4c-3007-baed-c3e606acd0af"
+        },
+        "user": {
+            "id": "5b66e6f6423b0a0001882971",
+            "image": "https://img.xiaohongshu.com/avatar/5d62943c12351d00013cc520.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "一只熊",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5db7824c000000000100310b",
+        "title": "减肥套餐",
+        "type": "video",
+        "likes": 309,
+        "isLiked": true,
+        "cover": {
+            "url": "img/ms8.jpg",
+            "width": 1080,
+            "height": 1920,
+            "fileId": "73caa1f8-2249-38cb-ba83-861236916dbd"
+        },
+        "user": {
+            "id": "5ce3e2b9000000001601e9f8",
+            "image": "https://img.xiaohongshu.com/avatar/5dcf4c34f31df200011f20fd.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "麦特顿",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5db7e37b0000000001005b17",
+        "title": "营养便当！",
+        "type": "normal",
+        "likes": 381,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms9.jpg",
+            "width": 1079,
+            "height": 1438,
+            "fileId": "971ccac1-07e7-30a0-a072-777d15ec19b1"
+        },
+        "user": {
+            "id": "5af289b14eacab19e3c5136b",
+            "image": "img/ms10.jpg",
+            "nickname": "旭晨",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5db7e12c000000000100575e",
+        "title": "招牌烧鸭",
+        "type": "normal",
+        "likes": 373,
+        "isLiked": true,
+        "cover": {
+            "url": "img/ms1.jpg",
+            "width": 1280,
+            "height": 1706,
+            "fileId": "63841e69-f915-3e92-b0e0-577835621815"
+        },
+        "user": {
+            "id": "5c315a550000000006031475",
+            "image": "https://img.xiaohongshu.com/avatar/5dce7f550eae5e0001fe48f3.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "daisy",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dcfc22e00000000010043b8",
+        "title": "吃了停不下来的霸王蟹❗️吃货无限回购款😋",
+        "type": "normal",
+        "likes": 364,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms2.jpg",
+            "width": 1280,
+            "height": 1705,
+            "fileId": "e03bef64-2eee-3789-ba48-da083135998e"
+        },
+        "user": {
+            "id": "5ac46cc0e8ac2b533d374630",
+            "image": "https://img.xiaohongshu.com/avatar/5ac46d1114de412eca1af482.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "瑾宁妈妈",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dcf5c2d0000000001009456",
+        "title": "🔥零厨艺❗️免打发❗️厨房小白也能轻松搞定的牛排🥞，新鲜美味",
+        "type": "normal",
+        "likes": 61,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms3.jpg",
+            "width": 1280,
+            "height": 1706,
+            "fileId": "7fcac969-592a-385d-bd27-a76d8e0c9dca"
+        },
+        "user": {
+            "id": "5b90881fd68e4900012d7a26",
+            "image": "https://img.xiaohongshu.com/avatar/5c62a25b6281e4000180add2.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "营养搭配均匀的早餐",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dceb7d00000000001003787",
+        "title": "北方的老铁们，这是你们喜欢的哦！",
+        "type": "normal",
+        "likes": 60,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms4.jpg",
+            "width": 1080,
+            "height": 1080,
+            "fileId": "63d94abd-b79e-32b5-97ea-47435c5e0ee0"
+        },
+        "user": {
+            "id": "5b2341bde8ac2b6cf730adf3",
+            "image": "https://img.xiaohongshu.com/avatar/5c773e991306d00001f850e8.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "妈咪很ok",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dce811f000000000100a3b5",
+        "title": "好吃不胖❗做法简单🔥超可爱营养早餐",
+        "type": "normal",
+        "likes": 60,
+        "isLiked": true,
+        "cover": {
+            "url": "img/ms5.jpg",
+            "width": 1280,
+            "height": 1706,
+            "fileId": "2821ce43-d7b8-3cb2-a2e3-9edb9157a103"
+        },
+        "user": {
+            "id": "5afea18511be1007defd9c1d",
+            "image": "https://img.xiaohongshu.com/avatar/5d5613671d314d0001fcf3eb.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "食荤者小厨房",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5dd1b1680000000001006451",
+        "title": "虾饺，其实它是我的最爱哦！你们喜欢吗？",
+        "type": "normal",
+        "likes": 12,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms6.jpg",
+            "width": 1280,
+            "height": 960,
+            "fileId": "3fb03b6f-6e22-346a-a60e-21d506f026c7"
+        },
+        "user": {
+            "id": "599b00e750c4b45b6728b109",
+            "image": "https://img.xiaohongshu.com/avatar/5d85d5efd2c53d00017a0d9e.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "May娘",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5db7df5000000000010029fd",
+        "title": "牛油果美食！",
+        "type": "normal",
+        "likes": 345,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms7.jpg",
+            "width": 1280,
+            "height": 1280,
+            "fileId": "d61e95e7-2d4c-3007-baed-c3e606acd0af"
+        },
+        "user": {
+            "id": "5b66e6f6423b0a0001882971",
+            "image": "https://img.xiaohongshu.com/avatar/5d62943c12351d00013cc520.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "一只熊",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5db7824c000000000100310b",
+        "title": "减肥套餐",
+        "type": "video",
+        "likes": 309,
+        "isLiked": true,
+        "cover": {
+            "url": "img/ms8.jpg",
+            "width": 1080,
+            "height": 1920,
+            "fileId": "73caa1f8-2249-38cb-ba83-861236916dbd"
+        },
+        "user": {
+            "id": "5ce3e2b9000000001601e9f8",
+            "image": "https://img.xiaohongshu.com/avatar/5dcf4c34f31df200011f20fd.jpg@80w_80h_90q_1e_1c_1x.jpg",
+            "nickname": "麦特顿",
+            "officialVerified": false
+        }
+    },
+    {
+        "id": "5db7e37b0000000001005b17",
+        "title": "营养便当！",
+        "type": "normal",
+        "likes": 381,
+        "isLiked": false,
+        "cover": {
+            "url": "img/ms9.jpg",
+            "width": 1079,
+            "height": 1438,
+            "fileId": "971ccac1-07e7-30a0-a072-777d15ec19b1"
+        },
+        "user": {
+            "id": "5af289b14eacab19e3c5136b",
+            "image": "img/ms10.jpg",
+            "nickname": "旭晨",
+            "officialVerified": false
+        }
+    },
+
+
+
+
+
+
+]
